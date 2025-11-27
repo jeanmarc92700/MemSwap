9241afb3ae2a2004755937fda6a6ed152e8bdd1a
MENDES Jean Marc OBS/OBF <BRCD7542@orange.com>
2025-11-27 15:50:49 +0100

    add test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50">
     <nav class="flex gap-4 p-4 bg-white shadow-md">
+      <h1>FRONTEND DEPLOYÉ !</h1>
       <router-link to="/calendar" class="text-lg font-medium text-gray-600 hover:text-blue-600 transition duration-150" :class="{ 'text-blue-600 font-bold border-b-2 border-blue-600': $route.path === '/calendar' }">
         📅 Calendrier
       </router-link>
